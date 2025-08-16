5873391b2bf8fcdef79587cfb099c9f2b11f526a
Shen <solove5171@gmail.com>
2025-08-16 10:44:33 +0800

    feat: 新增Footer組件並在App中引入

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "./component/Navbar.jsx";
 import Car from "./features/cart/cart.jsx";
 import Hero from "./component/Hero.jsx";
+import Footer from "./component/Footer.jsx";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
             <Navbar />
             <Hero />
             <Car />
+            <Footer />
         </>
     );
 }
