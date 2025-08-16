3e197400e2c4bf979721a995c8c289bbe278ae5e
Shen <solove5171@gmail.com>
2025-08-16 10:37:57 +0800

    feat: 更新Navbar組件，新增Logo與導航按鈕

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,8 +1,26 @@
 function Navbar() {
     return (
-        <nav className="bg-gray-800 p-4">
-            <h1 className="text-white text-2xl">Shopping Cart</h1>
-        </nav>
+        <div className="bg-slate-400 flex items-center p-4 h-30">
+            <img
+                src="img/wannabakerylogo.jpg"
+                alt="logo"
+                className="h-20 w-20  mr-2"
+            />
+            <h1 className="text-white text-5xl text-center font-bold caveat-regular">
+                WannaBackery
+            </h1>
+            <nav className="ml-auto flex space-x-4">
+                <button className="bg-white hover:bg-slate-300 text-slate-400 hover:text-white py-1 px-4 rounded text-2xl">
+                    回首頁
+                </button>
+                <button className="bg-white hover:bg-slate-300 text-slate-400 hover:text-white py-1 px-4 rounded text-2xl">
+                    登入
+                </button>
+                <button className="bg-white hover:bg-slate-300 text-slate-400 hover:text-white py-1 px-4 rounded text-2xl">
+                    購物車
+                </button>
+            </nav>
+        </div>
     );
 }
 
