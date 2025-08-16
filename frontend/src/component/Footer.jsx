85b415ba8709a46219e969ca42683fe53da0692f
Shen <solove5171@gmail.com>
2025-08-16 12:51:25 +0800

    feat: 調整Footer組件的樣式，新增上邊距以改善佈局

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="bg-slate-300 p-4">
+        <div className="bg-slate-300 p-4 mt-50">
             <p className="text-center text-slate-600">
                 &copy; 2023 WannaBackery. All rights reserved.
             </p>
