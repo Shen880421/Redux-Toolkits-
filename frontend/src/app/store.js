bb9bfea3114c2b35248ab6a67b0a2e8e0894c62d
Shen <solove5171@gmail.com>
2025-08-18 22:30:17 +0800

    feat: 將customerReducer新增至Redux store，以管理客戶狀態

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
+import customerReducer from "../features/customer/customerSlice";
 
 export const store = configureStore({
     reducer: {
         cart: cartReducer,
+        customer: customerReducer,
     },
 });
