d1cd8af77622476c0b1f90ce6ca18d715f21d7a5
Shen <solove5171@gmail.com>
2025-09-22 15:02:06 +0800

    feat: 新增 coupons 和 shipments 應用程式的基本結構，包括模型、管理、視圖和測試檔案，刪除promotions APP

--- backend/coupons/apps.py
+++ backend/coupons/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PromotionsConfig(AppConfig):
+class CouponsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'promotions'
+    name = 'coupons'
